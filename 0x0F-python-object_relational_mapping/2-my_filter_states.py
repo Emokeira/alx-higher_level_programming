03aee947d128b9324d3e9e8f8cf0d45a82a5b789
Edith Mokeira <edithmokeira7@gmail.com>
2024-04-15 15:14:33 +0300

    Added: A script that filters states by user input.

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-""" Lists all states with a name starting with N (upper N)
-from the database hbtn_0e_0_usa.
+"""
+Takes in an argument and sisplays all values in the states table of
+hbtn_0e_0_usa where the name matches the argument.
 """
 
 from sys import argv
@@ -15,5 +16,6 @@ if __name__ == "__main__":
             db=argv[3],
             charset='utf8')
     cur = db.cursor()
-    cur.execute("SELECT * FROM `states` ORDER BY `id`")
-    [print(state) for state in cur.fetchall() if stste[1][0] == "N"]
+    cur.execute("SELECT *\
+                FROM `states` WHERE BINARY `name` = '{}'".format(argv[4]))
+    [print(state) for state in cur.fetchall()]
